54175b093302da82af2172099a1196a4767696d8
PaikBean <PaikBean@users.noreply.github.com>
2025-01-28 15:43:54 +0000

    deploy: 46cb5c5817ee2dc893d6205f9cd5d04945f9c598

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1453122930"
+    serviceWorkerVersion: "2333542005"
   }
 });
